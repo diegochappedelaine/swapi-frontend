86785b15069175eb708f9e523dd8b760cb773bc3
Diego Chapedelaine <25375651+diegochappedelaine@users.noreply.github.com>
2021-09-12 14:05:43 +0200

    /films integration

--- src/pages/ListPage/FilmsPage.tsx
+++ src/pages/ListPage/FilmsPage.tsx
@@ -1,13 +1,66 @@
-import { useFetch } from "hooks";
+import { useEffect } from "react";
+import {
+  Loading,
+  SearchResultElement,
+  ListPageLayout,
+  Pagination,
+} from "components";
+import { useFetchLazy } from "hooks";
+import { retreiveDataTypeAndIdFromUrl, retreiveLastParamFromUrl } from "utils";
+import { UnorderedList } from "@chakra-ui/react";
+
 import { API_GET_FILM } from "api/end-points";
-import { GetFilms } from "types";
+import { GetFilms, SwapiRessources } from "types";
 
 const FilmsPage = () => {
-  const { data, loading, error } = useFetch<GetFilms>(API_GET_FILM);
+  const { data, loading, error, fetchData } = useFetchLazy<GetFilms>();
 
-  console.log(data);
+  useEffect(() => {
+    fetchData(API_GET_FILM);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
-  return <div></div>;
+  if (error) return <p>Error</p>;
+
+  return (
+    <ListPageLayout title="Films">
+      {loading && <Loading />}
+      <UnorderedList styleType={"none"} ml={0}>
+        {!loading &&
+          data?.results?.map((result, index) => {
+            const { id, dataType } = retreiveDataTypeAndIdFromUrl(result.url);
+            return (
+              <SearchResultElement
+                key={index}
+                url={`/${dataType}/${id}`}
+                element={result}
+                dataType={dataType as SwapiRessources}
+              />
+            );
+          })}
+      </UnorderedList>
+      {data && (
+        <Pagination
+          canPrevious={!!data?.previous}
+          canNext={!!data?.next}
+          toPrevious={() => {
+            if (data?.previous) {
+              fetchData(
+                `${API_GET_FILM}${retreiveLastParamFromUrl(data.previous)}`
+              );
+            }
+          }}
+          toNext={() => {
+            if (data?.next) {
+              fetchData(
+                `${API_GET_FILM}${retreiveLastParamFromUrl(data.next)}`
+              );
+            }
+          }}
+        />
+      )}
+    </ListPageLayout>
+  );
 };
 
 export default FilmsPage;
